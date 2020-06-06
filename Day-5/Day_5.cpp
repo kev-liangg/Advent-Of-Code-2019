6a280d4dfb24cd0444765e5164bfdbfaa5483dcf
Kevin Liang <kev.liang@utexas.edu>
2020-06-06 00:58:00 -0700

    Day 5 Part 2 completed

--- Day-5/Day_5.cpp
+++ Day-5/Day_5.cpp
@@ -35,9 +35,11 @@ void parseOpcode (int &opcode, int &mode1, int &mode2, int &mode3);
  * runs the opcode as specified by index on the machine instructions, updating
  * the entire input representing the indexed memory block
  *
+ * part 2: index can be modified by "jumps" within the opcodes
+ *
  * returns the "program counter increment," the offset to the next opcode
  */
-int runOpcode (std::vector<int> &inputVals, int index, int input);
+int runOpcode (std::vector<int> &inputVals, int &index, int input);
 
 int main () {
     std::ifstream inFile ("input.txt");
@@ -50,15 +52,23 @@ int main () {
     }
     // obtain deep copy of this original vector for part 2:
     std::vector<int> inputOriginal;
-    inputOriginal.assign(inputVals.begin (), inputVals.end ());
+    inputOriginal.assign (inputVals.begin (), inputVals.end ());
 
     /* Part 1: -------------------------------------------------------------- */
 
-    // initial conditions: position 1 with val 12, position 2 with val 2
+    // initial conditions: input = 1
     processInput (inputVals, 1);
 
     printf ("Part 1 Solution: See last nonzero output\n");
 
+    /* Part 2: -------------------------------------------------------------- */
+
+    inputVals.assign (inputOriginal.begin (), inputOriginal.end ());
+
+//     initial conditions: input = 5
+    processInput (inputVals, 5);
+
+    printf ("Part 2 Solution: See last output\n");
 }
 
 void processInput (std::vector<int> &inputVals, int input) {
@@ -66,6 +76,7 @@ void processInput (std::vector<int> &inputVals, int input) {
     int i = 0;
     while (i < inputVals.size ()) {
         // execute the opcode with helper function
+//        printf("running opcode with index %d\n", i);
         int offset = runOpcode (inputVals, i, input);
         i += offset;
     }
@@ -87,7 +98,7 @@ void parseOpcode (int &opcode, int &mode1, int &mode2, int &mode3) {
     opcode = opcodeFinal;
 }
 
-int runOpcode (std::vector<int> &inputVals, int index, int input) {
+int runOpcode (std::vector<int> &inputVals, int &index, int input) {
     // offset program counter, varies from how many inputs used for op
     int offset = 0;
     // get raw opcode from input
@@ -95,6 +106,14 @@ int runOpcode (std::vector<int> &inputVals, int index, int input) {
     // obtain param modes and final opcode from parse helper
     int mode1, mode2, mode3;
     parseOpcode (opcode, mode1, mode2, mode3);
+
+    // assume fetch max of 3 immediate params to simplify following switch cases
+    int param [3];
+    for (int i = 0; i + index + 1 < inputVals.size() && i < 3; i++) {
+        param[i] = inputVals.at (i + index + 1);
+    }
+    // must access final 3 values based on the utilized params for each case
+    int val1, val2, val3;
     switch (opcode) {
         // opcode 99: halt, return size to break out of caller loop
         case 99 :
@@ -102,48 +121,75 @@ int runOpcode (std::vector<int> &inputVals, int index, int input) {
             break;
         // opcode 1 and 2 both valid opcodes from Day 2
         case 1 :
-        case 2 : {
-            // get value of next 3 params for the operation
-            int param1 = inputVals.at (index + 1);
-            int param2 = inputVals.at (index + 2);
-            int writeIndex = inputVals.at (index + 3);
-            // actual values for operation differs by mode: 0->index, 1->immediate
-            int val1 = mode1 ? param1 : inputVals.at (param1);
-            int val2 = mode2 ? param2 : inputVals.at (param2);
-            // opcode 1: add vals; otherwise opcode 2: multiply vals
-            int toWrite = (opcode == 1) ? val1 + val2 : val1 * val2;
-            // write calculated value to index; always in indexing mode
-            inputVals.at (writeIndex) = toWrite;
+        case 2 :
+            // val1, val2 used
+            val1 = mode1 ? param[0] : inputVals.at (param[0]);
+            val2 = mode2 ? param[1] : inputVals.at (param[1]);
+            // opcode 1: add vals; otherwise opcode 2: multiply final vals
+            inputVals.at (param[2]) = (opcode == 1) ? val1 + val2 : val1 * val2;
             offset = 4;
             break;
-        }
-        // part 1, opcode 3: write to position given by single param
-        case 3 : {
-            int writeIndex = inputVals.at (index + 1);
-            inputVals.at (writeIndex) = input;
+        // part 1, opcode 3: write to position given by immediate param
+        case 3 :
+            inputVals.at (param[0]) = input;
             offset = 2;
             break;
-        }
         // part 1, opcode 4: "output" from single param and mode
-        case 4 : {
-            int param1 = inputVals.at (index + 1);
-            // value to output differs by mode: 0->index, 1->intermediate
-            int valOut = mode1 ? param1 : inputVals.at (param1);
-            printf("output: %d\n", valOut);
+        case 4 :
+            // process and print final value of param[0]
+            val1 = mode1 ? param[0] : inputVals.at (param[0]);
+            printf("output: %d\n", val1);
             offset = 2;
             break;
-        }
-        // part 2, opcode 5:
+        // part 2, opcode 5: if first param nonzero, set pc using second param
         case 5 :
+            val1 = mode1 ? param[0] : inputVals.at (param[0]);
+            val2 = mode2 ? param[1] : inputVals.at (param[1]);
+            // check final value of first param, update pc accordingly
+            if (val1 != 0) {
+                index = val2;
+            }
+            else {
+                offset = 3;
+            }
             break;
-        // part 2, opcode 6:
+        // part 2, opcode 6: if first param zero, set pc using second param
         case 6 :
+            val1 = mode1 ? param[0] : inputVals.at (param[0]);
+            val2 = mode2 ? param[1] : inputVals.at (param[1]);
+            // check final value of first param, update pc accordingly
+            if (val1 == 0) {
+                index = val2;
+            }
+            else {
+                offset = 3;
+            }
             break;
-        // part 2, opcode 7:
+        // part 2, opcode 7: if first param < second param, 1 in third param
         case 7 :
+            val1 = mode1 ? param[0] : inputVals.at (param[0]);
+            val2 = mode2 ? param[1] : inputVals.at (param[1]);
+            // compare final values of first and second params
+            if (val1 < val2) {
+                inputVals.at (param[2]) = 1;
+            }
+            else {
+                inputVals.at (param[2]) = 0;
+            }
+            offset = 4;
             break;
-        // part 2, opcode 8:
+        // part 2, opcode 8: if first param == second param, 1 in third param
         case 8 :
+            val1 = mode1 ? param[0] : inputVals.at (param[0]);
+            val2 = mode2 ? param[1] : inputVals.at (param[1]);
+            // compare final values of first and second params
+            if (val1 == val2) {
+                inputVals.at (param[2]) = 1;
+            }
+            else {
+                inputVals.at (param[2]) = 0;
+            }
+            offset = 4;
             break;
         default :
             printf ("invalid opcode, error in input\n");
