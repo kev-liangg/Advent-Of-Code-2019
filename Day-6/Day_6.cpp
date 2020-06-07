83ad64e78fa066c8aa95bf4e9b0006797b7c1e2e
Kevin Liang <kev.liang@utexas.edu>
2020-06-07 15:23:10 -0700

    finished processing functions, to fix duplicate allocation bug

--- Day-6/Day_6.cpp
+++ Day-6/Day_6.cpp
@@ -7,15 +7,19 @@
  */
 
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <iostream>
 #include <fstream>
 
 /*
  * each object contains a list of all the other objects that directly orbit it.
+ * an object also stores the object that it directly orbits.
  */
 struct Object {
+    std::string name;
     std::vector<Object*> orbits;
+    Object *orbiting;
 };
 
 /*
@@ -25,34 +29,81 @@ struct Object {
  * returns the pointer of the object under the name.
  */
 Object* findObject (std::unordered_map<std::string, Object*> &objects,
-                  std::string objectName, Object *pResult);
+                  std::string objectName);
+
+/*
+ * recursively totals the number of direct and indirect orbits for an object
+ */
+int countOrbits (Object *base);
+
+/*
+ * recursively determines the center of mass object, the one object that does
+ * not orbit any other object. takes any object in the list of input.
+ */
+Object* findRoot (Object *child);
 
 int main () {
     // mapping of an object's name to the location of its corresponding struct
     std::unordered_map<std::string, Object*> objects;
 
-    std::ifstream inFile ("input.txt");
+    std::ifstream inFile ("inputSmall.txt");
     std::string line;
     while (std::getline (inFile, line)) {
         // object on the right directly orbits object on the left
         std::string base = line.substr (0, 3);
         std::string orbiter = line.substr (4, 7);
+        Object *objectBase = findObject (objects, base);
+        Object *objectOrbiter = findObject (objects, orbiter);
+        // link the two objects by their direct orbit
+        objectBase->orbits.push_back (objectOrbiter);
+        objectOrbiter->orbiting = objectBase;
+        objectBase->name = base;
+        objectOrbiter->name = orbiter;
+        std::cout << base << " " << objectBase << "\n";
+        std::cout << orbiter << "\n" << objectOrbiter << "\n";
+    }
 
-        Object *toAddBase;
-        Object *toAddOrbiter;
+    /* Part 1: -------------------------------------------------------------- */
 
+    // determine center of mass object, call helper function with any map value
+    Object *root = findRoot (objects.begin ()->second);
+    // count all orbits of the center of mass for solution
+    printf ("Part 1 Solution: %d\n", countOrbits (root));
+}
+
+int countOrbits (Object *base) {
+    int numDirect = base->orbits.size ();
+    // count indirect orbits by calling recursively on all direct orbits
+    int numIndirect = 0;
+    for (Object *direct : base->orbits) {
+        numIndirect += countOrbits (direct);
+    }
+    return numDirect + numIndirect;
+}
+
+Object* findRoot (Object *child) {
+    std::cout << child->name << " ";
+    std::cout << child << "\n";
+    // simple recursive call from child objects until highest parent is reached
+    if (child->orbiting == nullptr) {
+        return child;
+    }
+    else {
+        return findRoot (child->orbiting);
     }
 }
 
 Object* findObject (std::unordered_map<std::string, Object*> &objects,
-                  std::string objectName) {
+                    std::string objectName) {
     // iterator for unordered map lookup
     std::unordered_map<std::string, Object*>::const_iterator search;
-    objects.find (objectName);
+    search = objects.find (objectName);
 
     Object *pObject;
     if (search == objects.end ()) {
         pObject = new Object;
+        std::pair<std::string, Object*> toInsert (objectName, pObject);
+        objects.insert (toInsert);
     }
     else {
         pObject = search->second;
