7ea04442db55b3f6ab762ba4267bc5562fc1555e
Kevin Liang <kev.liang@utexas.edu>
2020-06-09 20:37:41 -0700

    Finished Part 1

--- Day-9/Day_9.cpp
+++ Day-9/Day_9.cpp
@@ -32,7 +32,7 @@ int runOpcode (std::vector<long> &inputVals, int &index, long input,
                long &output, int &relativeBase);
 
 int main () {
-    std::ifstream inFile ("inputOld.txt");
+    std::ifstream inFile ("input.txt");
     // add each input value to vector for indexed read/write operations
     std::vector<long> inputVals;
     std::string val;
@@ -41,7 +41,7 @@ int main () {
         inputVals.push_back (std::stol (val));
     }
     // obtain deep copy of this original vector for part 2:
-    std::vector<int> inputOriginal;
+    std::vector<long> inputOriginal;
     inputOriginal.assign (inputVals.begin (), inputVals.end ());
 
     /* Part 1: -------------------------------------------------------------- */
@@ -54,11 +54,6 @@ int main () {
     /* Part 2: -------------------------------------------------------------- */
 
     inputVals.assign (inputOriginal.begin (), inputOriginal.end ());
-
-//     initial conditions: input = 5
-    processInput (inputVals, 5);
-
-    printf ("Part 2 Solution: See last output\n");
 }
 
 void processInput (std::vector<long> &inputVals, long input) {
@@ -90,6 +85,22 @@ void parseOpcode (long &opcode, int &mode1, int &mode2, int &mode3) {
     opcode = opcodeFinal;
 }
 
+long accessInput (std::vector<long> &inputVals, int index) {
+    // bounds checking: index cannot be negative
+    if (index < 0) {
+        return -99999;
+    }
+    // higher than given input, resize and return 0
+    else if (index >= inputVals.size ()) {
+        inputVals.resize (index);
+        return 0;
+    }
+    // within bounds
+    else {
+        return inputVals.at (index);
+    }
+}
+
 // helper function to return the actual value for a parameter access
 long getVal (std::vector<long> &inputVals, long param, int mode,
              int &relativeBase) {
@@ -101,19 +112,7 @@ long getVal (std::vector<long> &inputVals, long param, int mode,
     else if (mode == 0 || mode == 2) {
         // calculate index to access, mode 0 absolute position
         int indexAccess = !mode ? param : param + relativeBase;
-        // bounds checking: cannot be negative
-        if (indexAccess < 0) {
-            printf ("invalid input\n");
-            return -99999;
-        }
-        // higher than given input, return 0 as value
-        else if (indexAccess >= inputVals.size ()) {
-            return 0;
-        }
-        // within bounds
-        else {
-            return inputVals.at (indexAccess);
-        }
+        return accessInput (inputVals, indexAccess);
     }
 
     // invalid mode
@@ -130,9 +129,8 @@ int runOpcode (std::vector<long> &inputVals, int &index, long input,
     // get raw opcode from input
     long opcode = inputVals.at (index);
     // obtain param modes and final opcode from parse helper
-    int mode1, mode2, mode3;
+    int mode1, mode2, mode3, writeIndex;
     parseOpcode (opcode, mode1, mode2, mode3);
-
     // assume fetch max of 3 immediate params to simplify following switch cases
     long param [3];
     for (int i = 0; i + index + 1 < inputVals.size() && i < 3; i++) {
@@ -150,29 +148,26 @@ int runOpcode (std::vector<long> &inputVals, int &index, long input,
         // opcode 1 and 2 both valid opcodes from Day 2
         case 1 :
         case 2 :
-            // val1, val2 used
-//            val1 = mode1 ? param[0] : inputVals.at (param[0]);
-//            val2 = mode2 ? param[1] : inputVals.at (param[1]);
+            // determine write index if in relative mode
+            writeIndex = mode3 == 2 ? param[2] + relativeBase : param[2];
             // opcode 1: add vals; otherwise opcode 2: multiply final vals
-            inputVals.at (param[2]) = (opcode == 1) ? val1 + val2 : val1 * val2;
+            inputVals.at (writeIndex) = (opcode == 1) ? val1 + val2 : val1 * val2;
             offset = 4;
             break;
         // part 1, opcode 3: write to position given by immediate param
         case 3 :
-            inputVals.at (param[0]) = input;
+            // determine write index if in relative mode
+            writeIndex = mode1 == 2 ? param[0] + relativeBase : param[0];
+            inputVals.at (writeIndex) = input;
             offset = 2;
             break;
         // part 1, opcode 4: "output" from single param and mode
         case 4 :
-            // process and print final value of param[0]
-//            val1 = mode1 ? param[0] : inputVals.at (param[0]);
-            printf("output: %d\n", val1);
+            std::cout << "output: " << val1 << "\n";
             offset = 2;
             break;
         // part 2, opcode 5: if first param nonzero, set pc using second param
         case 5 :
-//            val1 = mode1 ? param[0] : inputVals.at (param[0]);
-//            val2 = mode2 ? param[1] : inputVals.at (param[1]);
             // check final value of first param, update pc accordingly
             if (val1 != 0) {
                 index = val2;
@@ -183,8 +178,6 @@ int runOpcode (std::vector<long> &inputVals, int &index, long input,
             break;
         // part 2, opcode 6: if first param zero, set pc using second param
         case 6 :
-//            val1 = mode1 ? param[0] : inputVals.at (param[0]);
-//            val2 = mode2 ? param[1] : inputVals.at (param[1]);
             // check final value of first param, update pc accordingly
             if (val1 == 0) {
                 index = val2;
@@ -195,29 +188,34 @@ int runOpcode (std::vector<long> &inputVals, int &index, long input,
             break;
         // part 2, opcode 7: if first param < second param, 1 in third param
         case 7 :
-//            val1 = mode1 ? param[0] : inputVals.at (param[0]);
-//            val2 = mode2 ? param[1] : inputVals.at (param[1]);
+            // determine write index if in relative mode
+            writeIndex = mode3 == 2 ? param[2] + relativeBase : param[2];
             // compare final values of first and second params
             if (val1 < val2) {
-                inputVals.at (param[2]) = 1;
+                inputVals.at (writeIndex) = 1;
             }
             else {
-                inputVals.at (param[2]) = 0;
+                inputVals.at (writeIndex) = 0;
             }
             offset = 4;
             break;
         // part 2, opcode 8: if first param == second param, 1 in third param
         case 8 :
-//            val1 = mode1 ? param[0] : inputVals.at (param[0]);
-//            val2 = mode2 ? param[1] : inputVals.at (param[1]);
+            // determine write index if in relative mode
+            writeIndex = mode3 == 2 ? param[2] + relativeBase : param[2];
             // compare final values of first and second params
             if (val1 == val2) {
-                inputVals.at (param[2]) = 1;
+                inputVals.at (writeIndex) = 1;
             }
             else {
-                inputVals.at (param[2]) = 0;
+                inputVals.at (writeIndex) = 0;
             }
             offset = 4;
+            break;
+        // Day 9: adjusts the relative base from value of only parameter
+        case 9 :
+            relativeBase += val1;
+            offset = 2;
             break;
         default :
             printf ("invalid opcode, error in input\n");
