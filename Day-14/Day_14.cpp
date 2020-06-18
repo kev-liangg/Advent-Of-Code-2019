ed71e140206217e41785205c502613c7322c8ccd
Kevin Liang <kev.liang@utexas.edu>
2020-06-17 23:38:18 -0700

    pass small test case 1, todo debug for larger

--- Day-14/Day_14.cpp
+++ Day-14/Day_14.cpp
@@ -20,38 +20,101 @@ typedef std::pair<std::string, int> req;
  */
 typedef std::unordered_map<std::string, std::vector<req>> MatInfo;
 
+/*
+ * hash mapping of a material requirement to an individual count
+ */
+typedef std::unordered_map<std::string, int> ReqCount;
+
 void processLine (std::string &in, std::vector<req> &reqs);
 
-int calcOres (std::string &name, MatInfo &mats, int num);
+int calcOres (MatInfo &mats, ReqCount &currReqs,
+              ReqCount &excesses);
 
 int main () {
-    std::ifstream fileIn ("inputSmall.txt");
+    std::ifstream fileIn ("input.txt");
     std::string line;
     MatInfo mats;
 
+    // track excess materials, map all to zero during input parse
+    ReqCount excesses;
     while (std::getline (fileIn, line)) {
         std::vector<req> reqs;
         processLine (line, reqs);
         // add to map
         std::string name = reqs.back ().first;
         mats.insert ({name, reqs});
+        excesses.insert ({name, 0});
     }
 
     /* Part 1: -------------------------------------------------------------- */
     MatInfo::iterator search = mats.find ("FUEL");
     std::vector<req> fuelReqs = search->second;
-    // caller: calculate requirement for every component of fuel
+    fuelReqs.pop_back ();
+
+    ReqCount currReqs;
     for (req r : fuelReqs) {
-        std::cout << r.first << " " << r.second << "\n";
+        currReqs.insert ({r.first, r.second});
     }
+    // run calculation function
+    int totalOres = calcOres (mats, currReqs, excesses);
+    std::cout << "Part 1 Solution: " << totalOres << std::endl;
 }
 
-int calcOres (std::string &name, MatInfo &mats, int num) {
-    MatInfo::iterator search = mats.find (name);
-    std::vector<req> reqs = search->second;
-    for (req r : reqs) {
+int calcOres (MatInfo &mats, ReqCount &currReqs,
+              ReqCount &excesses) {
+    // iterative solution
+    int ore = 0;
+    while (currReqs.size ()) {
+        ReqCount::iterator curr = currReqs.begin();
+        std::string name = curr->first;
+        int numRequired = curr->second;
+        std::cout << name << " " << numRequired << "\n";
 
+        // update required with excess
+        ReqCount::iterator searchCount = excesses.find (name);
+        numRequired -= searchCount->second;
+        searchCount->second = 0;
+
+        // get total materials needed to produce current requirement
+        MatInfo::iterator searchReqs = mats.find (name);
+        std::vector<req> reqs = searchReqs->second;
+        // determine total number material output required, scale factor
+        int output = reqs.back ().second;
+        reqs.pop_back ();
+
+        // check if excess generated while calculating scale factor
+        int scale = numRequired / output;
+        int excess = 0;
+        if (numRequired % output) {
+            scale += 1;
+            excess = numRequired - output * scale;
+        }
+
+        // remove current requirement and update excess
+        std::cout << currReqs.size();
+        currReqs.erase (name);
+        std::cout << " " << currReqs.size() << "\n";
+        searchCount->second = searchCount->second + excess;
+
+        for (req r : reqs) {
+            std::cout << r.first << "\n";
+            // base case: check for ore requirement
+            if (r.first == "ORE") {
+                ore += scale * r.second;
+            }
+            // otherwise add to map of current requirements
+            else {
+                searchCount = currReqs.find (r.first);
+                if (searchCount == currReqs.end ()) {
+                    currReqs.insert ({r.first, scale * r.second});
+                }
+                else {
+                    searchCount->second += scale * r.second;
+                }
+            }
+        }
     }
+    return ore;
 }
 
 void processLine (std::string &in, std::vector<req> &reqs) {
