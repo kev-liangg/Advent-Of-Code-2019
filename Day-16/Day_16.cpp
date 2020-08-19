7a930458be0d4bffc04568e0e0f20671b193db4e
Kevin Liang <kev.liang@utexas.edu>
2020-08-18 19:03:12 -0500

    transform function skeleton

--- Day-16/Day_16.cpp
+++ Day-16/Day_16.cpp
@@ -8,14 +8,30 @@
 #include <fstream>
 #include <vector>
 
+/*
+ * Apply the transformation pattern on the list of numbers
+ */
+void transformList (std::vector<short> &list);
+
 int main () {
 	// generate initial list by parsing complete string
 	std::ifstream inFile ("input.txt");
 	std::string input;
 	std::getline (inFile, input);
-	std::vector<char> list;
+	std::vector<short> list;
 	list.reserve (input.size());
 	for (char s : input) {
 		list.push_back(s - 48);
+	}
+}
+
+void transformList (std::vector<short> &list) {
+	// generate all n values of the new list
+	std::vector<short> temp;
+	temp.reserve(list.size());
+	short newVal;
+	for (int i = 0; i < list.size(); ++i) {
+		// todo: generate the new val using accumulate+pattern
+		temp.push_back(newVal);
 	}
 }
