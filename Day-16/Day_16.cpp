4d027b62b9d52ff044f4ba7703884965c634b836
Kevin Liang <kev.liang@utexas.edu>
2020-08-18 21:38:12 -0500

    pattern generation skeleton

--- Day-16/Day_16.cpp
+++ Day-16/Day_16.cpp
@@ -11,27 +11,36 @@
 /*
  * Apply the transformation pattern on the list of numbers
  */
-void transformList (std::vector<short> &list);
+void transformList (std::vector<int> &list);
+
+/*
+ * Generate the pattern for the applied transformation for each position
+ */
+void generatePattern (std::vector<int> &pattern, int pos);
 
 int main () {
 	// generate initial list by parsing complete string
 	std::ifstream inFile ("input.txt");
 	std::string input;
 	std::getline (inFile, input);
-	std::vector<short> list;
+	std::vector<int> list;
 	list.reserve (input.size());
 	for (char s : input) {
 		list.push_back(s - 48);
 	}
 }
 
-void transformList (std::vector<short> &list) {
+void transformList (std::vector<int> &list) {
 	// generate all n values of the new list
-	std::vector<short> temp;
+	std::vector<int> temp;
 	temp.reserve(list.size());
-	short newVal;
+	int newVal;
 	for (int i = 0; i < list.size(); ++i) {
 		// todo: generate the new val using accumulate+pattern
 		temp.push_back(newVal);
 	}
+}
+
+void generatePattern (std::vector<int> &pattern, int pos) {
+
 }
