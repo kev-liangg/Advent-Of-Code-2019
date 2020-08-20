5bfb17a9703c5dc5b779d791e3f7617234d00cc8
Kevin Liang <kev.liang@utexas.edu>
2020-08-19 23:20:28 -0500

    implement pattern generation

--- Day-16/Day_16.cpp
+++ Day-16/Day_16.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <deque>
 
 /*
  * Apply the transformation pattern on the list of numbers
@@ -16,7 +17,7 @@ void transformList (std::vector<int> &list);
 /*
  * Generate the pattern for the applied transformation for each position
  */
-void generatePattern (std::vector<int> &pattern, int pos);
+void generatePattern (std::vector<int> &pattern, size_t pos);
 
 int main () {
 	// generate initial list by parsing complete string
@@ -35,12 +36,26 @@ void transformList (std::vector<int> &list) {
 	std::vector<int> temp;
 	temp.reserve(list.size());
 	int newVal;
-	for (int i = 0; i < list.size(); ++i) {
+	for (size_t i = 0; i < list.size(); ++i) {
 		// todo: generate the new val using accumulate+pattern
 		temp.push_back(newVal);
 	}
 }
 
-void generatePattern (std::vector<int> &pattern, int pos) {
-
+void generatePattern (std::vector<int> &pattern, size_t pos) {
+	// base pattern: 0, 1, 0, -1
+	int base[] = {0, 1, 0, -1};
+	std::deque<int> temp;
+	size_t i = 0;
+	int baseIndex = 0;
+	while (i < pattern.size()) {
+		// expand base pattern from position
+		for (size_t j = 0; j <= pos; ++j) {
+			temp.push_back(base[baseIndex % 4]);
+			++i;
+		}
+		++baseIndex;
+	}
+	temp.pop_front();
+	std::copy (temp.begin(), temp.end(), pattern.begin());
 }
